251443c20820dd9c4f24e49de6eccaec44c3e527
Giorgi Abesadze <44198003+giorgiabes@users.noreply.github.com>
2024-06-17 09:01:53 +0000

    rendering collections

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
-export default function App() {
+const App = (props) => {
+  const { notes } = props;
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <div className="max-w-4xl mx-auto p-5">
+      <h1 className="text-3xl font-bold underline mb-4">Notes</h1>
+      <ul className="pl-5 list-none">
+        <li className="mb-5">{notes[0].content}</li>
+        <li className="mb-5">{notes[1].content}</li>
+        <li className="mb-5">{notes[2].content}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default App;
