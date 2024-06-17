5a1afdc431477a086b087c7670652db0de52ae97
Giorgi Abesadze <44198003+giorgiabes@users.noreply.github.com>
2024-06-17 09:10:21 +0000

    map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,10 @@ const App = (props) => {
     <div className="max-w-4xl mx-auto p-5">
       <h1 className="text-3xl font-bold underline mb-4">Notes</h1>
       <ul className="pl-5 list-none">
-        <li className="mb-5">{notes[0].content}</li>
-        <li className="mb-5">{notes[1].content}</li>
-        <li className="mb-5">{notes[2].content}</li>
+        {notes.map(note => 
+          <li key={note.id}>
+            {note.content}
+          </li>)}
       </ul>
     </div>
   );
